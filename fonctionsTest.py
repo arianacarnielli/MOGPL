44e414e0d8c0807dea25c5ebf9151d4f27c70168
Ariana Carnielli <ariana.carnielli@gmail.com>
2019-12-06 00:50:24 +0100

    presque fini. Code et interface operationnels

--- fonctionsTest.py
+++ fonctionsTest.py
@@ -12,9 +12,27 @@ plt.rcParams.update({"pgf.texsystem": "pdflatex"})
 
 from tqdm import tqdm
 from jeu import JeuSequentiel, JeuSimultane
+from strategie import StrategieAveugle, StrategieOptimaleSequentielle,\
+ StrategieAleatoire, StrategieHumaine, StrategieOptimaleSimultaneeTour,\
+ StrategieOptimaleSimultanee
 
 def EvaluationGainSequentiel(listeStrat, D, N, nbFois):
     """
+    Calcule les gains moyens en nbFois jeux séquentiels entre les stratégies 
+    listées dans listeStrat.
+    
+    Args :
+        listeStrat : tableau avec les stratégies (pas encore initialisées) 
+            qu'on veut tester.
+        D : Nombre maximum de dés qu'un joueur peut lancer dans 1 tour du 
+            jeu.
+        N : Nombre de points à atteindre pour gagner une partie. 
+        nbFois : Quantité de jeux joués pour remplir chaque case.
+
+    Returns :
+        Un tableau de taille listeStrat**2 où chaque case (i, j) représente le
+        gain moyen en nbFois jeux de la stratégie listeStrat[i] contre la 
+        stratégie listeStrat[j].
     """
     jeu = JeuSequentiel(D, N)
     res = np.zeros((len(listeStrat), len(listeStrat)))
@@ -27,6 +45,21 @@ def EvaluationGainSequentiel(listeStrat, D, N, nbFois):
 
 def EvaluationGainSimultane(listeStrat, D, N, nbFois):
     """
+    Calcule les gains moyens en nbFois jeux simultanés entre les stratégies 
+    listées dans listeStrat.
+    
+    Args :
+        listeStrat : tableau avec les stratégies (pas encore initialisées) 
+            qu'on veut tester.
+        D : Nombre maximum de dés qu'un joueur peut lancer dans 1 tour du 
+            jeu.
+        N : Nombre de points à atteindre pour gagner une partie. 
+        nbFois : Quantité de jeux joués pour remplir chaque case.
+
+    Returns :
+        Un tableau de taille listeStrat**2 où chaque case (i, j) représente le
+        gain moyen en nbFois jeux de la stratégie listeStrat[i] contre la 
+        stratégie listeStrat[j].
     """
     jeu = JeuSimultane(D, N)
     res = np.zeros((len(listeStrat), len(listeStrat)))
@@ -41,6 +74,25 @@ def EvaluationGainSimultane(listeStrat, D, N, nbFois):
 
 def EvaluationVictoiresFonctionD(TypeJeu, S1, S2, Dvect, N, nbFois):
     """
+    Calcule deux tableaux avec le nombre de victoires du joueur 1 et du 
+    joueur 2 avec des stratégies fixes et en variant D, le nombre maximum de 
+    dés. On joue nbFois pour chaque case.
+    
+     Args :
+        TypeJeu : le type de Jeu qu'on veut jouer (Simultané ou Séquentiel).
+            Pas initialisé.
+        S1 : Une stratégie. Pas initialisé.
+        S2 : Une stratégie. Pas initialisé.
+        Dvect : Tableau avec les nombres maximums de dés qu'un joueur peut 
+            lancer dans 1 tour du jeu. Pour chaque case de Dvect, la fonction 
+            joue nbFois fois.
+        N : Nombre de points à atteindre pour gagner une partie. 
+        nbFois : Quantité de jeux joués pour remplir chaque case.
+
+    Returns :
+        Un couple de tableaux de même taille que Dvect où chaque case i 
+        représente la quantité de victoires en nbFois jeux de la stratégie S1 
+        contre la stratégie S2. 
     """
     nbVic1 = np.zeros(Dvect.shape)
     nbVic2 = np.zeros(Dvect.shape)
@@ -61,6 +113,25 @@ def EvaluationVictoiresFonctionD(TypeJeu, S1, S2, Dvect, N, nbFois):
 
 def EvaluationVictoiresFonctionN(TypeJeu, S1, S2, D, Nvect, nbFois):
     """
+    Calcule deux tableaux avec le nombre de victoires du joueur 1 et du 
+    joueur 2 avec des stratégies fixes et en variant N, le nombre de points à 
+    atteindre pour gagner une partie. On joue nbFois pour chaque case.
+    
+     Args :
+        TypeJeu : le type de Jeu qu'on veut jouer (Simultané ou Séquentiel).
+            Pas initialisé.
+        S1 : Une stratégie. Pas initialisé.
+        S2 : Une stratégie. Pas initialisé.
+        D : Nombre maximum de dés qu'un joueur peut lancer dans 1 tour du 
+            jeu.
+        Nvect : Tableau avec les nombres de points à atteindre pour gagner une 
+            partie. Pour chaque case de Nvect, la fonction joue nbFois fois. 
+        nbFois : Quantité de jeux joués pour remplir chaque case.
+
+    Returns :
+        Un couple de tableaux de même taille que Nvect où chaque case i 
+        représente la quantité de victoires en nbFois jeux de la stratégie S1 
+        contre la stratégie S2. 
     """
     nbVic1 = np.zeros(Nvect.shape)
     nbVic2 = np.zeros(Nvect.shape)
@@ -78,3 +149,16 @@ def EvaluationVictoiresFonctionN(TypeJeu, S1, S2, D, Nvect, nbFois):
                 nbVic2[i] += 1
             jeu._reset()
     return nbVic1, nbVic2
+
+if __name__=="__main__":
+    D = 10
+    N = 100
+    jeu = JeuSimultane(D, N)
+    strategieAv = StrategieAveugle(jeu)
+    strategieO = StrategieOptimaleSequentielle(jeu)
+    strategieAlea = StrategieAleatoire(jeu)
+    strategieH = StrategieHumaine(jeu)
+    strategieOST = StrategieOptimaleSimultaneeTour(jeu)
+    strategieOS = StrategieOptimaleSimultanee(jeu)
+    jeu.setStrategies(strategieOS, strategieH)
+    jeu.jouer()
--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  5 18:28:00 2019
+Created on Thu Dec  1 18:28:00 2019
 
-@author: arian
+@author:Ariana Carnielli \\ Ivan Kachaikin
+Interface graphique pour le projet de MOGPL 2019-2020
 """
 
 import PySimpleGUI as sg  
@@ -53,24 +54,137 @@ while True:
         jeu = JeuSimultane(vals1[2], vals1[0])
         if vals1[1] != 'Humaine' and vals1[3] != 'Humaine':   
             
-            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(88, 20))]]  
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]  
             strat1 = strats[vals1[1]](jeu)
             strat2 = strats[vals1[3]](jeu)
             jeu.setStrategies(strat1, strat2)
             win1.Hide()
             win2 = sg.Window('Dice Battle', layout2, finalize = True)  
             jeu.jouer()
+            
         elif vals1[1] == 'Humaine' and vals1[3] != 'Humaine':
-            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(88, 20))], \
-                        [sg.Spin(values=[i for i in range(1, jeu.D + 1)])]]
-            
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
             strat2 = strats[vals1[3]](jeu)
-            strat2 = strats[vals1[3]](jeu)
-            jeu.setStrategies(strat1, strat2)
             win1.Hide()
-            win2 = sg.Window('Dice Battle', layout2, finalize = True)  
-            jeu.jouer()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
             
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+
+            while not jeu._estfini():
+                layout3 = [[sg.Text('Joueur 1 :')],\
+                        [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                initial_value = 1, size = (5, 1))],\
+                        [sg.Button('jouer dés')]]
+                win3 = sg.Window('Dés', layout3, finalize = True)
+                while True:
+                    ev3, vals3 = win3.Read(timeout=100)  
+                    if ev3 is None:  
+                        break  
+                    if ev3 == 'jouer dés':
+                        des1 = vals3[0]
+                        win3.Close()
+                        break
+                if ev3 is None:
+                    break
+                des2 = strat2.jouerTour(jeu.joueur2, jeu.joueur1)
+                
+                jeu.joueur1 += jeu._tirageDes(des1)
+                jeu.joueur2 += jeu._tirageDes(des2)
+    
+                print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")
+        elif vals1[1] != 'Humaine' and vals1[3] == 'Humaine':
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
+            strat1 = strats[vals1[1]](jeu)
+            win1.Hide()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
+            
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+            
+            while not jeu._estfini():
+                layout3 = [[sg.Text('Joueur 2 :')],\
+                        [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                initial_value = 1, size = (5, 1))],\
+                        [sg.Button('jouer dés')]]
+                win3 = sg.Window('Dés', layout3, finalize = True)
+                while True:
+                    ev3, vals3 = win3.Read(timeout=100)  
+                    if ev3 is None:  
+                        break  
+                    if ev3 == 'jouer dés':
+                        des2 = vals3[0]
+                        win3.Close()
+                        break
+                if ev3 is None:
+                    break
+                des1 = strat1.jouerTour(jeu.joueur1, jeu.joueur2)
+                
+                jeu.joueur1 += jeu._tirageDes(des1)
+                jeu.joueur2 += jeu._tirageDes(des2)
+    
+                print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")
+        else:
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
+            win1.Hide()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
+            
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+            
+            while not jeu._estfini():
+                layout3 = [[sg.Text('Joueur 1 :')],\
+                        [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                initial_value = 1, size = (5, 1))],\
+                        [sg.Button('jouer dés')]]
+                win3 = sg.Window('Dés', layout3, finalize = True)
+                while True:
+                    ev3, vals3 = win3.Read(timeout=100)  
+                    if ev3 is None:  
+                        break  
+                    if ev3 == 'jouer dés':
+                        des1 = vals3[0]
+                        win3.Close()
+                        break
+                if ev3 is None:  
+                    break  
+                layout3 = [[sg.Text('Joueur 2 :')],\
+                        [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                initial_value = 1, size = (5, 1))],\
+                        [sg.Button('jouer dés')]]
+                win3 = sg.Window('Dés', layout3, finalize = True)
+                while True:
+                    ev3, vals3 = win3.Read(timeout=100)  
+                    if ev3 is None:  
+                        break  
+                    if ev3 == 'jouer dés':
+                        des2 = vals3[0]
+                        win3.Close()
+                        break
+                if ev3 is None:  
+                    break  
+                jeu.joueur1 += jeu._tirageDes(des1)
+                jeu.joueur2 += jeu._tirageDes(des2)
+    
+                print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")
         while True:  
             ev2, vals2 = win2.Read()  
             if ev2 is None or ev2 == 'Exit':  
@@ -81,14 +195,161 @@ while True:
             
     elif ev1 == 'Jeu Séquentiel' and not win2_active:  
         win2_active = True  
-        layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(88, 20))]]  
         jeu = JeuSequentiel(vals1[2], vals1[0])
-        strat1 = strats[vals1[1]](jeu)
-        strat2 = strats[vals1[3]](jeu)
-        jeu.setStrategies(strat1, strat2)
-        win1.Hide()  
-        win2 = sg.Window('Dice Battle', layout2, finalize = True)  
-        jeu.jouer()
+        if vals1[1] != 'Humaine' and vals1[3] != 'Humaine':   
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]  
+            strat1 = strats[vals1[1]](jeu)
+            strat2 = strats[vals1[3]](jeu)
+            jeu.setStrategies(strat1, strat2)
+            win1.Hide()  
+            win2 = sg.Window('Dice Battle', layout2, finalize = True)  
+            jeu.jouer()
+         
+        elif vals1[1] == 'Humaine' and vals1[3] != 'Humaine':
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
+            strat2 = strats[vals1[3]](jeu)
+            win1.Hide()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
+            
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+            joueurCourant = 1
+            
+            while not jeu._estfini():
+                if joueurCourant == 1:
+                    layout3 = [[sg.Text('Joueur 1 :')],\
+                            [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                    initial_value = 1, size = (5, 1))],\
+                            [sg.Button('jouer dés')]]
+                    win3 = sg.Window('Dés', layout3, finalize = True)
+                    while True:
+                        ev3, vals3 = win3.Read(timeout=100)  
+                        if ev3 is None:  
+                            break  
+                        if ev3 == 'jouer dés':
+                            des1 = vals3[0]
+                            win3.Close()
+                            joueurCourant = 2
+                            break
+                    if ev3 is None:  
+                        break  
+                    jeu.joueur1 += jeu._tirageDes(des1)
+                    print("Le joueur 1 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+                else:
+                    des2 = strat2.jouerTour(jeu.joueur2, jeu.joueur1)
+                    jeu.joueur2 += jeu._tirageDes(des2)
+                    print("Le joueur 2 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+                    joueurCourant = 1
+                
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")
+        elif vals1[1] != 'Humaine' and vals1[3] == 'Humaine':
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
+            strat1 = strats[vals1[1]](jeu)
+            win1.Hide()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
+            
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+            joueurCourant = 1
+            
+            while not jeu._estfini():
+                if joueurCourant == 1:
+                    des1 = strat1.jouerTour(jeu.joueur1, jeu.joueur2)
+                    jeu.joueur1 += jeu._tirageDes(des1)
+                    joueurCourant = 2
+                    print("Le joueur 1 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+                else:
+                    layout3 = [[sg.Text('Joueur 2 :')],\
+                            [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                    initial_value = 1, size = (5, 1))],\
+                            [sg.Button('jouer dés')]]
+                    win3 = sg.Window('Dés', layout3, finalize = True)
+                    while True:
+                        ev3, vals3 = win3.Read(timeout=100) 
+                        if ev3 is None:  
+                            break  
+                        if ev3 == 'jouer dés':
+                            des2 = vals3[0]
+                            win3.Close()
+                            joueurCourant = 1
+                            break
+                    if ev3 is None:  
+                        break  
+                    jeu.joueur2 += jeu._tirageDes(des2)
+                    print("Le joueur 2 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+                
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")
+        else:
+            layout2 = [[sg.Text('Jeu :')], [sg.Output(size=(50, 20))]]
+            win1.Hide()
+            win2 = sg.Window('Dice Battle', layout2, finalize = True) 
+            
+            #on recrée le jeu à la main, pas à pas :
+            print ("La partie commence :")
+            print("Joueur 1 | Joueur 2")
+            joueurCourant = 1
+            
+            while not jeu._estfini():
+                if joueurCourant == 1:
+                    layout3 = [[sg.Text('Joueur 1 :')],\
+                            [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                    initial_value = 1, size = (5, 1))],\
+                            [sg.Button('jouer dés')]]
+                    win3 = sg.Window('Dés', layout3, finalize = True)
+                    while True:
+                        ev3, vals3 = win3.Read(timeout=100)
+                        if ev3 is None:  
+                            break  
+                        if ev3 == 'jouer dés':
+                            des1 = vals3[0]
+                            win3.Close()
+                            break
+                    joueurCourant = 2
+                    if ev3 is None:  
+                        break  
+                    jeu.joueur1 += jeu._tirageDes(des1)
+                    print("Le joueur 1 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+                else:
+                    layout3 = [[sg.Text('Joueur 2 :')],\
+                            [sg.Spin(values=[i for i in range(1, jeu.D + 1)],\
+                                    initial_value = 1, size = (5, 1))],\
+                            [sg.Button('jouer dés')]]
+                    win3 = sg.Window('Dés', layout3, finalize = True)
+                    while True:
+                        ev3, vals3 = win3.Read(timeout=100)  
+                        if ev3 is None:  
+                            break  
+                        if ev3 == 'jouer dés':
+                            des2 = vals3[0]
+                            win3.Close()
+                            break
+                    joueurCourant = 1
+                    if ev3 is None:  
+                        break
+                    jeu.joueur2 += jeu._tirageDes(des2)
+                    print("Le joueur 2 joue :")
+                    print("{:8d} | {:8d}".format(jeu.joueur1, jeu.joueur2))
+
+            if jeu.vainqueur() == 0:
+                print("Match nul")    
+            else:
+                print("La partie est finie, le joueur", jeu.vainqueur(), \
+                      "a gagné")     
         
         while True:  
             ev2, vals2 = win2.Read()  
@@ -97,4 +358,5 @@ while True:
                 win2_active = False  
                 win1.UnHide()  
                 break
+        
 win1.close()
